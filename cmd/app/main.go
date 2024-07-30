7dd42d2e7556e9c29beed789d6f9f7102eeeedee
Kelvin Iria <iriakelvin@yahoo.com>
2024-07-30 02:03:16 +0000

    changes to main.go and include godotenv

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"log"
 	"log/slog"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
 	router := chi.NewMux()
 
 	router.Get("/foo", handleFoo)
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module gothstarter
 
 go 1.22.5
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require github.com/go-chi/chi/v5 v5.1.0
+
+require github.com/joho/godotenv v1.5.1 // indirect
